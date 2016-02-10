08f25c00124aaff3cf4edd1467bf02f517321ea6
jsamp701 <jsamp701@gmail.com>
2016-02-10 00:29:48 -0500

    Updated roadmap

--- roadmap-to-html.py
+++ roadmap-to-html.py
@@ -15,10 +15,12 @@ for line in sourcefile:
 		start = False
 	elif line[0] != "\t":
 		#output all the html
-		outfile.write('<tr><td class="auto-style1">TBD</td>')
-		outfile.write('<td><ul><li>' + section[0][:-1:] + '<ul>')
-		for strng in section[1::]:
-			outfile.write("<li>" + strng + "</li>")
+		date = section[1][1:-1:]
+		title = section[0][:-1:]
+		outfile.write('<tr><td class="auto-style1">' + date + '</td>')
+		outfile.write('<td><ul><li>' + title + '<ul>')
+		for strng in section[2::]:
+			outfile.write("<li>" + strng[1::] + "</li>")
 		outfile.write('</ul></li></ul></td></tr>')
 		#remake section
 		section = []
