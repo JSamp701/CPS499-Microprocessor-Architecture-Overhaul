21149964589228835e5a0ef09117c7211b4d9831
Joel Sampson <constant8change6@gmail.com>
2016-02-29 20:18:09 -0800

    Worked on CLR and CW.  Pushing for Security's Sake

--- AS_CLR/source/runner.py
+++ AS_CLR/source/runner.py
@@ -20,7 +20,7 @@ def create_and_configure_parser():
     parser = argparse.ArgumentParser()
     parser.description = "A command line runner for CPS310 ARMSimulator library projects."
     parser.add_argument("library", help="path to library file to simulate")
-    parser.add_argument("-l", "--load", help="name of elf file to load into memory",
+    parser.add_argument("-l", "--load", help="name of elf file to load into memory, required if no --test flag",
                         metavar="elf-name", dest="load_file_name")
     parser.add_argument("-m", "--mem", help="amount of ram (Bytes) to simulate", default=256, metavar="ram-amount",
                         dest="amount_of_ram", type=int)
@@ -30,6 +30,12 @@ def create_and_configure_parser():
                         default=0, type=int, metavar="cache-amount", dest="amount_to_cache")
     parser.add_argument("-t", "--trace", help="specify filename for tracing", metavar="trace-name",
                         dest="trace_file_name", default="trace.log")
+    parser.add_argument("--trace-level", help="specify level for tracing", metavar="trace-level",
+                        dest="trace_level", default=2, type=int)
+    parser.add_argument("--log", help="specify filename for logging", metavar="log-name",
+                        dest="debug_file_name")
+    parser.add_argument("--debug-level", help="specify level for logging", metavar="log-level",
+                        dest="log_level", default=1, type=int)
     parser.add_argument("-s", "--script", help="specify filename for interrupt scripting", metavar="script-name",
                         dest="script_file_name", default=False)
     return parser
--- AS_CW/source/as_lib_wrapper.c
+++ AS_CW/source/as_lib_wrapper.c
@@ -7,4 +7,38 @@
 
 static ARMSIM_CTX *ctx;
 static void *as_lib_handle;
+static ARMSIM_LOG_HOOK wrapper_logger;
+static void *wrapper_logger_ctx;
 
+// API Pointers
+static ARMSIM_STATUS (*create_simulator)(ARMSIM_CTX**, void*,ARMSIM_MMIO_STORE_HOOK, ARMSIM_MMIO_LOAD_HOOK);
+static ARMSIM_STATUS (*destroy_simulator)(ARMSIM_CTX*);
+static ARMSIM_STATUS (*set_log_level)(ARMSIM_CTX*, ARMSIM_LOGLEVEL);
+static ARMSIM_STATUS (*set_trace_level)(ARMSIM_CTX*, ARMSIM_TRACELEVEL);
+static ARMSIM_STATUS (*set_log_hook)(ARMSIM_CTX*, ARMSIM_LOG_HOOK, void*);
+static ARMSIM_STATUS (*set_trace_hook)(ARMSIM_CTX*, ARMSIM_LOG_HOOK, void*);
+static ARMSIM_STATUS (*get_ram_word)(ARMSIM_CTX*, ARM_ADDRESS, ARM_WORD*);
+static ARMSIM_STATUS (*set_ram_word)(ARMSIM_CTX*, ARM_ADDRESS, ARM_WORD);
+static ARMSIM_STATUS (*get_reg)(ARMSIM_CTX*, ARM_REG, ARM_WORD*);
+static ARMSIM_STATUS (*set_reg)(ARMSIM_CTX*, ARM_REG, ARM_WORD);
+static ARMSIM_STATUS (*get_mode)(ARMSIM_CTX*, ARM_MODE*);
+static ARMSIM_STATUS (*set_mode)(ARMSIM_CTX*, ARM_MODE);
+static ARMSIM_STATUS (*get_banked_reg)(ARMSIM_CTX*, ARM_BANKED_REG, ARM_WORD*);
+static ARMSIM_STATUS (*set_banked_reg)(ARMSIM_CTX*, ARM_BANKED_REG, ARM_WORD);
+static ARMSIM_STATUS (*reset_cpu)(ARMSIM_CTX*);
+static ARMSIM_STATUS (*enable_cache)(ARMSIM_CTX*, ARM_ADDRESS);
+static ARMSIM_STATUS (*invalidate_cache)(ARMSIM_CTX*, ARM_ADDRESS);
+static ARMSIM_STATUS (*notify_event)(ARMSIM_CTX*, ARMSIM_EVENT);
+static ARMSIM_STATUS (*execute)(ARMSIM_CTX*, size_t, size_t*);
+
+static const ASAPI_MAJOR LIB_MAJOR = ASAPI_MAJOR_0;
+static const enum ASAPI_MINOR LIB_MINOR = ASAPI_MINOR_2;
+
+char* initialize_library(char *lib_path, ARMSIM_LOG_HOOK log_hook, void *actx){
+    char *error;
+    wrapper_logger=log_hook;
+    wrapper_logger_ctx = actx;
+
+
+    as_lib_handle =
+}
